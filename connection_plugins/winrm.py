27d260e16027ab28680afccda615a05f0302f1c6
Chris Church <chris@ninemoreminutes.com>
2014-05-10 18:57:11 -0400

    More work on connection plugin, overriding default modules with .ps1 extension, simple stat module.

--- connection_plugins/winrm.py
+++ connection_plugins/winrm.py
@@ -29,9 +29,9 @@ from ansible import utils
 from ansible.callbacks import vvv, vvvv
 
 from winrm import Response
+from winrm.exceptions import WinRMTransportError
 from winrm.protocol import Protocol
 
-WINRM_CONNECTION_CACHE = {}
 
 class Connection(object):
     '''WinRM connections over HTTP/HTTPS.'''
@@ -39,20 +39,80 @@ class Connection(object):
     def __init__(self,  runner, host, port, user, password, *args, **kwargs):
         self.runner = runner
         self.host = host
-        self.port = port or 5985
+        self.port = port
         self.user = user
         self.password = password
         self.has_pipelining = False
         self.protocol = None
         self.shell_id = None
+        self.delegate = None
+
+        # Monkeypatch module finder to attempt to load modules with .ps1
+        # suffix first when using this connection plugin.
+        import inspect
+        from ansible.utils.plugins import module_finder
+        orig_find_plugin = module_finder.find_plugin
+        def find_plugin(name):
+            try_ps1_suffix = False
+            for frame_tuple in inspect.stack():
+                frame_locals = inspect.getargvalues(frame_tuple[0])[3]
+                if 'conn' in frame_locals:
+                    if frame_locals['conn'] is self:
+                        try_ps1_suffix = True
+                    break
+            module_path = None
+            if try_ps1_suffix and not name.lower().endswith('.ps1'):
+                module_path = orig_find_plugin('%s.ps1' % name)
+            if not module_path:
+                module_path = orig_find_plugin(name)
+            return module_path
+        module_finder.find_plugin = find_plugin
 
     def _winrm_connect(self):
-        vvv("ESTABLISH WINRM CONNECTION FOR USER: %s on PORT %s TO %s" % (self.user, self.port, self.host), host=self.host)
-        netloc = '%s:%d' % (self.host, self.port)
-        url = urlparse.urlunsplit(('http', netloc, '/wsman', '', ''))
-        return Protocol(url, username=self.user, password=self.password)
+        '''
+        Establish a WinRM connection over HTTP/HTTPS.
+        '''
+        # Ansible doesn't pass ansible_ssh_port to this connection, so we have
+        # to get it ourselves.
+        port = self.port
+        if not port:
+            host = self.delegate or self.host
+            host_vars = self.runner.inventory.get_variables(host, vault_password=self.runner.vault_pass)
+            port = int(host_vars.get('ansible_ssh_port', None) or 5986)
+        vvv("ESTABLISH WINRM CONNECTION FOR USER: %s on PORT %s TO %s" % \
+            (self.user, port, self.host), host=self.host)
+        netloc = '%s:%d' % (self.host, port)
+        transport_schemes = [('plaintext', 'https'), ('plaintext', 'http')] # FIXME: ssl/kerberos
+        if port == 5985:
+            transport_schemes = reversed(transport_schemes)
+        exc = None
+        for transport, scheme in transport_schemes:
+            endpoint = urlparse.urlunsplit((scheme, netloc, '/wsman', '', ''))
+            vvvv('WINRM CONNECT: transport=%s endpoint=%s' % (transport, endpoint),
+                 host=self.host)
+            protocol = Protocol(endpoint, transport=transport,
+                                username=self.user, password=self.password)
+            try:
+                protocol.send_message('')
+                return protocol
+            except WinRMTransportError, exc:
+                err_msg = str(exc.args[0])
+                if re.search(r'Operation\s+?timed\s+?out', err_msg, re.I):
+                    raise
+                m = re.search(r'Code\s+?(\d{3})', err_msg)
+                if m:
+                    code = int(m.groups()[0])
+                    if code == 411:
+                        return protocol
+                vvvv('WINRM CONNECTION ERROR: %s' % err_msg, host=self.host)
+                continue
+        if exc:
+            raise exc
 
     def _winrm_escape(self, value, include_vars=False):
+        '''
+        Return value escaped for use in PowerShell command.
+        '''
         # http://www.techotopia.com/index.php/Windows_PowerShell_1.0_String_Quoting_and_Escape_Sequences
         # http://stackoverflow.com/questions/764360/a-list-of-string-replacements-in-python
         subs = [('\n', '`n'), ('\r', '`r'), ('\t', '`t'), ('\a', '`a'),
@@ -65,19 +125,29 @@ class Connection(object):
         replace = lambda m: substs[m.lastindex - 1]
         return re.sub(pattern, replace, value)
 
+    def _winrm_get_script_cmd(self, script):
+        '''
+        Convert a PowerShell script to a single base64-encoded command.
+        '''
+        vvvv('WINRM SCRIPT: %s' % script, host=self.host)
+        encoded_script = base64.b64encode(script.encode('utf-16-le'))
+        return ['PowerShell', '-NoProfile', '-NonInteractive',
+                '-EncodedCommand', encoded_script]
+
     def _winrm_filter(self, cmd_parts):
         try:
             # HACK: Catch runner _make_tmp_path() call and replace with PS.
-            if cmd_parts[0] == 'mkdir' and cmd_parts[1] == '-p' and cmd_parts[-3] == '&&' and cmd_parts[-2] == 'echo' and cmd_parts[2] == cmd_parts[-1]:
+            if cmd_parts[0] == 'mkdir' and cmd_parts[1] == '-p' and cmd_parts[-3] == '&&' and cmd_parts[-2] == 'echo' and cmd_parts[2] == cmd_parts[-1] and '-tmp-' in os.path.basename(cmd_parts[2]):
                 basename = os.path.basename(cmd_parts[2])
                 script = '''(New-Item -Type Directory -Path $env:temp -Name "%s").FullName;''' % self._winrm_escape(basename)
                 return self._winrm_get_script_cmd(script)
             # HACK: Catch runner _execute_module() chmod calls and ignore.
-            if cmd_parts[0] == 'chmod' and len(cmd_parts) == 3:
+            if cmd_parts[0] == 'chmod' and '-tmp-' in cmd_parts[2]:
                 return [] # No-op.
             # HACK: Catch runner _remove_tmp_path() call and replace with PS.
-            if cmd_parts[0] == 'rm' and cmd_parts[1] == '-rf' and cmd_parts[2]:
-                script = '''Remove-Item "%s" -Force -Recurse;''' % self._winrm_escape(cmd_parts[2])
+            if cmd_parts[0] == 'rm' and cmd_parts[1] == '-rf' and '-tmp-' in cmd_parts[2]:
+                path = cmd_parts[2].replace('/', '\\')
+                script = '''Remove-Item "%s" -Force -Recurse;''' % self._winrm_escape(path)
                 return self._winrm_get_script_cmd(script)
             # HACK: Catch runner _remote_md5() call and replace with PS.
             if cmd_parts[0] == 'rc=0;' and '(/usr/bin/md5sum' in cmd_parts:
@@ -87,42 +157,33 @@ class Connection(object):
                 path = path.replace('/', '\\')
                 script = '''(Get-FileHash -Path "%s" -Algorithm MD5).Hash.ToLower();''' % self._winrm_escape(path)
                 return self._winrm_get_script_cmd(script)
-            # HACK: Catch 
-            if 'powershell' in cmd_parts:
+            # HACK: Catch the call to run the PS module.
+            if any(x.lower().startswith('powershell') for x in cmd_parts):
                 env_vars = {}
                 for n, cmd_part in enumerate(cmd_parts):
-                    if cmd_part == 'powershell':
+                    if cmd_part.lower().startswith('powershell'):
                         cmd_parts = cmd_parts[n:]
                         break
                     elif '=' in cmd_part:
                         var, val = cmd_part.split('=', 1)
                         env_vars[var] = val
-                new_cmd_parts = [
-                    'powershell',
-                    '-NoProfile', '-NonInteractive',
-                    '-File',
-                    '%s.ps1' % cmd_parts[1].replace('/', '\\'),
-                    cmd_parts[2].replace('/', '\\').rstrip(';'),
-                ]
-                if len(cmd_parts) >= 6 and cmd_parts[3] == 'rm' and False:
-                    new_cmd_parts.extend([
-                        'Remove-Item',
-                        self._winrm_escape(cmd_parts[5].replace('/', '\\')),
-                        '-Force',
-                        '-Recurse',
-                    ])
-                return new_cmd_parts
+                mod_path = cmd_parts[1].replace('/', '\\')
+                if not mod_path.lower().endswith('.ps1'):
+                    mod_path = '%s.ps1' % mod_path
+                args_path = cmd_parts[2].replace('/', '\\').rstrip(';')
+                script = '''PowerShell -NoProfile -NonInteractive -File "%s" "%s";''' % (self._winrm_escape(mod_path), self._winrm_escape(args_path))
+                if len(cmd_parts) >= 6 and cmd_parts[3] == 'rm' and cmd_parts[4] == '-rf' and '-tmp-' in cmd_parts[5]:
+                    path = cmd_parts[5].replace('/', '\\')
+                    script += ''' Remove-Item "%s" -Force -Recurse;''' % self._winrm_escape(path)
+                return self._winrm_get_script_cmd(script)
         except IndexError:
             pass
         return cmd_parts
 
-    def _winrm_get_script_cmd(self, script):
-        vvvv('WINRM SCRIPT %s' % script, host=self.host)
-        encoded_script = base64.b64encode(script.encode('utf-16-le'))
-        return ['PowerShell -NoProfile -NonInteractive -EncodedCommand %s' % encoded_script]
-
     def _winrm_exec(self, command, args):
         vvvv("WINRM EXEC %r %r" % (command, args), host=self.host)
+        if not self.protocol:
+            self.protocol = self._winrm_connect()
         if not self.shell_id:
             self.shell_id = self.protocol.open_shell()
         command_id = None
@@ -137,11 +198,8 @@ class Connection(object):
                 self.protocol.cleanup_command(self.shell_id, command_id)
 
     def connect(self):
-        cache_key = "%s__%d__%s__" % (self.host, self.port, self.user)
-        if cache_key in WINRM_CONNECTION_CACHE:
-            self.protocol = WINRM_CONNECTION_CACHE[cache_key]
-        else:
-            self.protocol = WINRM_CONNECTION_CACHE[cache_key] = self._winrm_connect()
+        # No-op. Connect lazily on first command, to allow for runner to set
+        # self.delegate, needed if actual host vs. host name are different.
         return self
 
     def exec_command(self, cmd, tmp_path, sudo_user=None, sudoable=False, executable='/bin/sh', in_data=None, su=None, su_user=None):
@@ -171,7 +229,7 @@ class Connection(object):
                 try:
                     out_data = in_file.read(chunk_size)
                     if offset == 0:
-                        if out_data.splitlines()[0].startswith('#!powershell'):
+                        if out_data.lower().startswith('#!powershell') and not out_path.lower().endswith('.ps1'):
                             out_path = out_path + '.ps1'
                         script = '''New-Item -Path "%s" -Type File -Value "%s";''' % (self._winrm_escape(out_path), self._winrm_escape(out_data, True))
                     else:
